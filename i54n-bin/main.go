8c88960c30826d24c66bd4ce5397020a3c5866a9
jane mf arc <jane.arc@pobox.com>
2025-03-26 07:11:48 -0700

    stubbing out

--- i54n-bin/main.go
+++ i54n-bin/main.go
@@ -13,9 +13,25 @@ type Verb interface {
 	Execute(args []string) error
 }
 
+type SimpleVerb struct {
+	name        string
+	executeFunc func(args []string) error
+}
+
+func (sv SimpleVerb) Name() string {
+	return sv.name
+}
+
+func (sv SimpleVerb) Execute(args []string) error {
+	return sv.executeFunc(args)
+}
+
+func NewSimpleVerb(name string, executeFunc func(args []string) error) Verb {
+	return SimpleVerb{name: name, executeFunc: executeFunc}
+}
+
 var verbRegistry = make(map[string]Verb)
 
-// RegisterVerb is called by each verb library to register its function.
 func RegisterVerb(v Verb) {
 	verbRegistry[v.Name()] = v
 }
@@ -24,12 +40,14 @@ func main() {
 	rootCmd := &cobra.Command{
 		Use:   "bior",
 		Short: "Bioreactor management tool",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
 	}
 
-	// Automatically add a Cobra command for each registered verb.
+	// Add all registered verbs as subcommands.
 	for _, verb := range verbRegistry {
-		// Capture the current verb to avoid closure issues.
-		v := verb
+		v := verb // capture range variable
 		cmd := &cobra.Command{
 			Use:   v.Name(),
 			Short: fmt.Sprintf("Execute the %s verb", v.Name()),
@@ -44,4 +62,31 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	RegisterVerb(NewSimpleVerb("about", func(args []string) error {
+		fmt.Println("bior is the ultimate bioreactor management tool, built for the Anthropocene.")
+		return nil
+	}))
+
+	RegisterVerb(NewSimpleVerb("license", func(args []string) error {
+		fmt.Println("Licensed under the MIT License.")
+		return nil
+	}))
+
+	RegisterVerb(NewSimpleVerb("license", func(args []string) error {
+		licenseBytes, err := os.ReadFile("LICENSE.md")
+		if err != nil {
+			return fmt.Errorf("error reading LICENSE.md: %v", err)
+		}
+		noticeBytes, err := os.ReadFile("NOTICE")
+		if err != nil {
+			return fmt.Errorf("error reading NOTICE: %v", err)
+		}
+		fmt.Println(string(licenseBytes))
+		fmt.Println()
+		fmt.Println(string(noticeBytes))
+		return nil
+	}))
 }
